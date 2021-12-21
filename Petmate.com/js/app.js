9885f9d516294b68c3f2bfa832a1dafde73956f8
karansalunkhe15 <89115640+karansalunkhe15@users.noreply.github.com>
2021-12-21 11:00:08 +0530

    Add files via upload

---
+++ Petmate.com/js/app.js
@@ -0,0 +1,38 @@
+function shopOpen() {
+  document.getElementById("footer_shop").style.display = "block";
+}
+
+function shopClose() {
+  document.getElementById("footer_shop").style.display = "none";
+}
+
+function suppClose() {
+  document.getElementById("footer_support").style.display = "none";
+}
+function suppOpen() {
+  document.getElementById("footer_support").style.display = "block";
+}
+
+function compOpen() {
+  document.getElementById("footer_company").style.display = "block";
+}
+
+function compClose() {
+  document.getElementById("footer_company").style.display = "none";
+}
+
+function AccOpen() {
+  document.getElementById("footer_Acc").style.display = "block";
+}
+
+function AccClose() {
+  document.getElementById("footer_Acc").style.display = "none";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // document.getElementById("b").style.background = "grey";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
